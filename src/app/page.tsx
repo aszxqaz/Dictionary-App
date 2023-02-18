89cea67aaa8e43b06bbff2c8cd89f5b4bec5d438
Maxim B <b.maxim.dev@gmail.com>
2023-02-18 06:46:05 +0400

    node-fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,38 @@
-import { query } from "@/dictionary-api/fetcher";
-import { NotFound } from "@/partials/NotFound/NotFound";
-import { QueryModeRadios } from "@/partials/QueryModeRadios/QueryModeRadios";
-import { SearchInput } from "@/partials/SearchInput/SearchInput";
-import { WordDefinition } from "@/partials/WordDefinition/WordDefinition";
+import { query } from '@/dictionary-api/fetcher'
+import { NotFound } from '@/partials/NotFound/NotFound'
+import { QueryModeRadios } from '@/partials/QueryModeRadios/QueryModeRadios'
+import { SearchInput } from '@/partials/SearchInput/SearchInput'
+import { WordDefinition } from '@/partials/WordDefinition/WordDefinition'
 
 export default async function Home({
-  params,
-  searchParams,
+	params,
+	searchParams,
 }: {
-  params: { slug: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
+	params: { slug: string }
+	searchParams?: { [key: string]: string | string[] | undefined }
 }) {
-  let data = null;
-  let param = "";
-  if (typeof searchParams?.word === "string") {
-    param = decodeURI(searchParams.word);
-  }
-  if (param) {
-    data = await query(param);
-  }
-  console.log(data);
-  // console.log(paramOrData);
-  return (
-    <main className="container">
-      <SearchInput initial={param} />
-      <div style={{ display: "flex" }}>
-        <QueryModeRadios />
-      </div>
-      {JSON.stringify(data, null , 2)}
-      {data && data[0] ? <WordDefinition word={data[0]} /> : <NotFound />}
-    </main>
-  );
+	let data = null
+	let param = ''
+	if (typeof searchParams?.word === 'string') {
+		param = decodeURI(searchParams.word)
+	}
+	if (param) {
+		data = await query(param)
+	}
+	console.log(data)
+	// console.log(paramOrData);
+	return (
+		<main className="container">
+			<SearchInput initial={param} />
+			<div style={{ display: 'flex' }}>
+				<QueryModeRadios />
+			</div>
+			{JSON.stringify(data, null, 2)}
+			{data && '0' in data && data[0] ? (
+				<WordDefinition word={data[0]} />
+			) : (
+				<NotFound />
+			)}
+		</main>
+	)
 }
--- src/dictionary-api/fetcher.ts
+++ src/dictionary-api/fetcher.ts
@@ -1,5 +1,10 @@
-export const query = async (word: string) => {
-  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-  console.log(url)
-  return fetch(url).then((r) => r.json()).catch(err => err)
-};
+import fetch from 'node-fetch'
+import { Word } from './types'
+
+export const query = async (word: string): Promise<Word[] | Error> => {
+	const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+	console.log(url)
+	return fetch(url)
+		.then((r) => r.json() as Promise<Word[]>)
+		.catch((err) => err)
+}
